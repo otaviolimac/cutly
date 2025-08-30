75e23297a63b35414919bf83d168f420756dc415
Otávio Lima <otavio-lima-co@gmail.com>
2025-08-30 12:20:30 -0300

    Initial commit

---
+++ cutly-backend/src/main/java/com/cutly/cutly_backend/CutlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cutly.cutly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutlyBackendApplication.class, args);
+	}
+
+}
---
+++ cutly-backend/src/test/java/com/cutly/cutly_backend/CutlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutly.cutly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
